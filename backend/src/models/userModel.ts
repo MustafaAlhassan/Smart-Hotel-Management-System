ea3937621363c0dfa512c29b38681c638dae1062
Mustafa Alhassan <mustafamusab266@gmail.com>
2025-11-23 22:47:04 +0300

    Implement login page frontend & backend with JWT auth

---
+++ backend/src/models/userModel.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+export enum UserRole {
+  ADMIN = "Admin",
+  RECEPTIONIST = "Receptionist",
+  MANAGER = "Manager",
+  HOUSEKEEPING = "Housekeeping",
+}
+
+export interface IUser extends Document {
+  _id: Types.ObjectId;
+  firstName: string;
+  lastName: string;
+  username: string;
+  role: UserRole;
+  email: string;
+  password: string;
+  isActive: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    role: {
+      type: String,
+      enum: Object.values(UserRole),
+      default: UserRole.HOUSEKEEPING,
+    },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isActive: { type: Boolean, required: true, default: true },
+  },
+  { timestamps: true }
+);
+
+export const UserModel = mongoose.model<IUser>("user", userSchema);
