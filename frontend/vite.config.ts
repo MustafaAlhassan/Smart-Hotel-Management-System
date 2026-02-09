53e1ead128621aec168de58e4e2f5ed692044cd2
IBO98X <ibrahim.qahtan919@gmail.com>
2026-02-09 18:15:45 +0300

    Link the front end with back end using api of rooms

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // Replace 4000 with your desired port
+  },
 });
